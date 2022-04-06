ca2547d57f57747d23f0574a2d58ab4254a33393
Mitchell Scott <misc4432@colorado.edu>
2022-04-05 19:56:41 -0600

    changes

--- install.bash
+++ install.bash
@@ -1,11 +1,13 @@
 
+echo -e "updating and installing dependencies"
 sudo apt update
 
 sudo xargs apt install -y --no-install-recommends dependencies.txt
 
 
+echo -e "installing pip and python requirements"
 
-curl https://bootstrap.pypa.io/pip/3.6/get-pip.py -o get-pip.py
+curl https://bootstrap.pypa.io/pip/3.9/get-pip.py -o get-pip.py
 
 sudo python3 get-pip.py
 
